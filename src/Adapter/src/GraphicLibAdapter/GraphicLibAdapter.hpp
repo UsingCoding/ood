a67784169494117b3cc3d84674f885cd501970e9
UsingCoding <extendedmoment@outlook.com>
2020-11-02 02:44:37 +0300

    Реализовал разделение для рисования

--- src/Adapter/src/GraphicLibAdapter/GraphicLibAdapter.hpp
+++ src/Adapter/src/GraphicLibAdapter/GraphicLibAdapter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include "../GraphicsLib/GraphicLib.hpp"
 #include "../ModernGraphicsLib/ModernGraphicsLib.hpp"
 
@@ -10,11 +11,19 @@ namespace app
     private:
         struct Coord
         {
+            Coord() {}
+            Coord(int x, int y) : m_x(x), m_y(y) {}
+
             int m_x, m_y;
 
             operator modern_graphics_lib::CPoint()
             {
                 return modern_graphics_lib::CPoint(m_x, m_y);
+            }
+
+            friend bool operator ==(const Coord & c1, const Coord & c2)
+            {
+                return c1.m_x == c2.m_x && c1.m_y == c2.m_y;
             }
 
             void SetNewCoords(int x, int y)
@@ -29,21 +38,38 @@ namespace app
 
         void MoveTo(int x, int y) override
         {
-            m_currCoord.SetNewCoords(x, y);
+            Coord coord(x, y);
+
+            if (m_beginCoords == std::nullopt)
+            {
+                m_beginCoords = coord;
+                m_currCoord = coord;
+                m_renderer.BeginDraw();
+                return;
+            }
+
+            m_currCoord = coord;
         }
 
         void LineTo(int x, int y) override
         {
-            m_renderer.BeginDraw();
+            Coord coord(x, y);
 
-            m_renderer.DrawLine(m_currCoord, modern_graphics_lib::CPoint(x, y));
+            m_renderer.DrawLine(m_currCoord.value(), coord);
 
-            m_renderer.EndDraw();
+            if (m_beginCoords.value() == coord)
+            {
+                m_renderer.EndDraw();
+                m_beginCoords = std::nullopt;
+                m_currCoord = std::nullopt;
+                return;
+            }
         }
 
     private:
         modern_graphics_lib::CModernGraphicsRenderer & m_renderer;
-        Coord m_currCoord;
+        std::optional<Coord> m_currCoord = std::nullopt;
+        std::optional<Coord> m_beginCoords = std::nullopt;
     };
 }
 
